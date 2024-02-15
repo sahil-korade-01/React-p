1ffd4fbdfbad55522881b5de0335f9ab162433b3
sahil-korade-01 <sahilkorade3@gmail.com>
2024-02-15 15:35:02 +0530

    web page

---
+++ web page/web page/src/compnents/Hero.jsx
@@ -0,0 +1,24 @@
+const Hero = () => {
+  return (
+    <main className="hero">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae culpa
+          eligendi provident tempora harum quis neque! Illo, aliquid. Tenetur
+          nostrum in asperiores iure ducimus odit consectetur eaque. Quaerat, at
+          modi.
+        </p>
+      </div>
+      <div className="hero-btn">
+        <button>Shop Now</button>
+        <button>Catagery</button>
+      </div>
+      <div className="shop">
+        <p>Available on</p>
+      </div>
+      <div className="hero-img"></div>
+    </main>
+  );
+};
+export default Hero;
---
+++ web page/web page/src/compnents/Nav.jsx
@@ -0,0 +1,20 @@
+const Nav = ()=>{
+    return<div>
+    <nav>
+      <div className="logo">
+        <img src="" alt="logo" /></div>
+      <ul>
+
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+
+      </ul>
+      <button>Login</button>
+
+    </nav>
+
+  </div>
+}
+export default Nav;
