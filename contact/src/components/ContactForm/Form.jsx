506fd2e645eb84dae1cb3eeaec63bcca2b84eef9
sahil-korade-01 <sahilkorade3@gmail.com>
2024-02-16 13:08:16 +0530

    form

---
+++ contact/src/components/ContactForm/Form.jsx
@@ -0,0 +1,17 @@
+import styles from './Form.module.css'
+
+const Form = () => {
+  return (
+    <section className={styles.container}>
+    <div className={styles.form}>  </div>
+    <div className={styles.img}>
+    
+    </div>
+    
+    
+    </section>
+
+  )
+}
+
+export default Form
